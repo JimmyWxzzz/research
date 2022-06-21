af4e25c40c179888a350e8785037acf1022e610b
JimmyWxzzz <xuzhuwang354@gmail.com>
2022-06-21 06:28:15 -0400

    somechange

---
+++ untitled/src/main/java/HelloWeb/boot/controller/controller.java
@@ -0,0 +1,14 @@
+package HelloWeb.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @RequestMapping("/hello")
+    public String handle01() {
+
+        return "Hello, SpringBoot2";
+    }
+}
