c00989fbcb2642fea4624762a733a137bd9cca3a
JimmyWxzzz <xuzhuwang354@gmail.com>
2022-08-19 17:00:39 -0400

    1

--- sweepy/src/main/java/com/sweepy/SweepyApplication.java
+++ sweepy/src/main/java/com/sweepy/SweepyApplication.java
@@ -3,11 +3,23 @@ package com.sweepy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 @SpringBootApplication
 public class SweepyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SweepyApplication.class, args);
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306","root","wangxuzhu354BJDX");
+            if (con.isValid(1000)){
+                System.out.println("Connected");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
 }
