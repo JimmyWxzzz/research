5384e269282a68dc50163b758e538fb4d7219c6c
JimmyWxzzz <xuzhuwang354@gmail.com>
2022-08-18 20:21:29 -0400

    1

---
+++ sweepy/src/test/java/com/sweepy/SweepyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sweepy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweepyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
